4c5fa36ff3870d662aeeb079bc4020f9edd4115a
wahajsaleem07 <69244032+wahajsaleem07@users.noreply.github.com>
2020-08-05 03:47:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
+
+require('./api/models/Country');
+
+const brandRoutes = require('./api/routes/brands');
+const productRoutes = require('./api/routes/product');
+const lookupRoutes = require('./api/routes/lookup');
+const userRoutes = require('./api/routes/user');
+const branchRoutes = require('./api/routes/branch');
+const productCategoriesRoutes = require('./api/routes/product-categories');
+const productTypeRoutes = require('./api/routes/product-type');
+const productOriginRoutes = require('./api/routes/product-origin');
+const supplierRoutes = require('./api/routes/supplier');
+
+mongoose.connect('mongodb+srv://the-medic-admin:' + process.env.MONGO_ATLAS_PW +'@the-medics-demo.nup3i.azure.mongodb.net/the-medics-demo?retryWrites=true&w=majority', {useNewUrlParser: true});
+
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Header", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    if(req.method === 'OPTIONS'){
+        res.header("Access-Control-Allow-Methods", "PUT. POST, GET, DELETE, PATCH");
+        return res.status(200).json({});
+    }
+    next();
+})
+
+//Routes
+app.use('/product', productRoutes);
+app.use('/lookup', lookupRoutes);
+app.use('/user', userRoutes);
+app.use('/branch', branchRoutes);
+app.use('/brand', brandRoutes);
+app.use('/product-categories', productCategoriesRoutes);
+app.use('/product-origin', productOriginRoutes);
+app.use('/product-type', productTypeRoutes);
+app.use('/supplier', supplierRoutes);
+
+//Error Handling
+app.use((req, res, next) => {
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error);
+})
+app.use((error, req, res, next) => {
+   res.status(error.status || 500);
+   
+   res.json({
+       error: {
+           message: error.message
+       }
+   })
+});
+
+module.exports = app;
